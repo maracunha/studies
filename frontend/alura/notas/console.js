0032ad1920f1f85ba8094a4da4cda0676dd2a934
Renan Marangon <maracunha+github@gmail.com>
2019-12-19 18:34:38 -0300

    Nodas - add

---
+++ frontend/alura/notas/console.js
@@ -0,0 +1,21 @@
+console.groupCollapsed("Nossas mensagens");
+console.log("estou escrevendo no console");
+console.error("eu sou um erro");
+console.warn("eu sou um aviso");
+var num = 12;
+var num = 12;
+if(num>10){
+    console.error
+("numero nao pode ser maior que 10");
+}
+console.assert(num<=10, 
+"numero nao pode ser maior que 10");
+console.groupEnd();
+
+console.table([
+  {nome:"Alura", num:35},
+  {nome:"Deco", num: 42}
+])
+
+// $ = document.querySelector()
+// $$ = document.querySelectorAll()
